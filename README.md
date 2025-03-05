c46eed91732727f5b529d0dad32755307334ee29
raccoonKannapit <raccoonKannapit@gmail.com>
2025-03-05 20:04:01 +0700

    ex3 and edit readme.md

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # hello
 
 this project is for check skill in __CSI204__
+btw i'm still new to Python
 
 ## this project is using these to make
->- **Languages**: JavaScript,HTML,CSS
+>- **Languages**: JavaScript,HTML,CSS,Python
 >- **Frameworks**: React,Node.js
 >- **Libraries**: Bootstrap,React-Bootstrap,React-Router-dom
 >- **Version Control**: Git & GitHub
--- src/page/Ex_3/ex3.jsx
+++ src/page/Ex_3/ex3.jsx
@@ -1,12 +1,60 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
+import './ex3.css'
 
 function CheckSkillEx3() {
     const navigate = useNavigate();
+
+    const [bgColor, setBgColor] = useState("")
+
+
+    const changeBgColor = () => {
+        document.body.style.backgroundColor = bgColor
+    }
+
+    const mouseOverHandler = () => {
+        console.log("mouse over")
+    }
+
+    const handleKeyUp = (event) => {
+        console.log("Key Pressed:", event.key) // Logs the key name
+    }
+
+    const [email, setEmail] = useState("")
+
+    const checkEmail = (email) => {
+        if (isEmailValid(email)) {
+            console.log("Email is valid");
+        } else {
+            console.log("Email is invalid");
+        }
+    }
+    const isEmailValid = (email) => {
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        return regex.test(email) && (email.endsWith(".com") || email.endsWith(".net"));
+      };
 
     return ( 
         <div>
             <button className="btn btn-primary home-button" onClick={() => navigate("/")}>Home</button>
             <h1>Ex3</h1>
+            <h3>part 1</h3>
+            <label >color input : </label>
+            <input type="text" value={bgColor} onChange={(e) => setBgColor(e.target.value)} /> <br />
+            <button className="btn btn-success mt-3" onClick={changeBgColor} >Submit</button>
+            <hr />
+            <h3>part 2</h3>
+            <p className="text-hover" onMouseOver={() => mouseOverHandler()}>try hovering over this text</p>
+            <input type="text" onKeyUp={handleKeyUp} placeholder="Press any key" />
+            <hr />
+            <h3>part 3</h3>
+            <form action="">
+                <label>Email : </label>
+                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br />
+                <button className="btn btn-success mt-3" onClick={() => checkEmail(email)} >Submit</button>
+            </form>
+
         </div>
      );
 }
